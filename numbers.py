8800aba6c4ff851b18b0f4ba34a67b5b9145c8d2
Alex Moreno <alex.moreno.costa@gmail.com>
2019-05-07 23:09:39 -0300

    Trabalhando com variaveis

---
+++ numbers.py
@@ -0,0 +1,23 @@
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ variables.py
@@ -0,0 +1,12 @@
+x = 5
+y = "Jhon"
+print(x)
+print(y)
+
+concat = "Awesome"
+print("Python is " + concat)
+
+first = "Python is "
+second = "Awesome"
+third = first + second
+print(third)
